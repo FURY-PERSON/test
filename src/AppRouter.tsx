121e4e2fe496d6c0b75771403ad4703b0bc3baca
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2021-09-15 21:15:03 +0300

    feat: add not found page

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch, useLocation } from "react-router";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { AddNewNotePage } from "./components/pages/addNewNotePage/addNewNotePage";
 import { NoteDetailPage } from "./components/pages/noteDetailPage/noteDetailPage";
+import { NotFoundPage } from "./components/pages/notFoundPage/notFoundPage";
 
 export default function AppRouter() {
   let location =  useLocation();
@@ -19,13 +20,13 @@ export default function AppRouter() {
           <Route exact path="/posts/:id">
             <NoteDetailPage></NoteDetailPage>
           </Route>
-{/*           <Route path="/notfound">
-            <NotFound></NotFound>
+          <Route path="/notfound">
+            <NotFoundPage></NotFoundPage>
           </Route>
-{          <Route exact path="/">
+{/*           <Route exact path="/">
             <Redirect to='/posts'></Redi>
-          </Route>}
-          <Redirect to='/notfound'></Redirect> */}
+          </Route> */}
+          <Redirect to='/notfound'></Redirect>
         </Switch>
       </CSSTransition>
     </TransitionGroup>
---
+++ src/components/pages/notFoundPage/notFoundPage.tsx
@@ -0,0 +1,9 @@
+export function NotFoundPage() {
+  return(
+    <div className="notFoundPage">
+      <div className="noteFoundPaage__wrapper">
+        <h1>Not Found</h1>
+      </div>
+    </div>
+  )
+}
